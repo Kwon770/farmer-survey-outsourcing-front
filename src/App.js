a0e15c93fc013480f7594bff9877995ab5aae835
Kwon770 <sckwon770@gmail.com>
2022-06-19 23:10:38 +0900

    chore: project environment init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {ThemeProvider, createGlobalStyle} from "styled-components";
+import reset from 'styled-reset';
+import Theme from "./styles/Theme"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Sector1";
+
+const AppRouter = () => (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+        </Routes>
+    </Router>
+)
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={Theme}>
+          <GlobalStyle />
+        <AppRouter/>
+      </ThemeProvider>
   );
 }
 
---
+++ src/pages/Sector1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Sector1 = () => {
+    return <Background></Background>
+}
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: gray;
+`;
+
+export default Sector1;
