fdf3adeae22c67c1dcc269c6502e0fe191fcdc37
Kwon770 <sckwon770@gmail.com>
2022-07-03 17:16:43 +0900

    feat: rr sector3

--- src/pages/Sector3.js
+++ src/pages/Sector3.js
@@ -69,7 +69,6 @@ const Sector3 = () => {
     const [s3b4b4, setS3b4b4] = useState('');
 
     const [s3b5, setS3b5] = useState('');
-    const [s3b5_Etc8, setS3b5_Etc8] = useState('');
     const [s3b6, setS3b6] = useState('');
     const [s3b7, setS3b7] = useState('');
 
@@ -156,6 +155,10 @@ const Sector3 = () => {
         setS3b4b3('');
 
         resetS3b4b3_23s();
+
+        setS3b5('');
+        setS3b6('');
+        setS3b7('');
     }
 
     const handleS3b4b1 = (e) => {
@@ -294,10 +297,10 @@ const Sector3 = () => {
                 <br/>
                 <br/>
 
-                농업 스타트업단지는 청년농업인의 영농기반을 마련하여 농촌에 안정적 으로 정착하고 농지확보 어려움을 해결하고자, 저활용 농지(유휴농지, 국·공유지 및 매입비축농지)를 활용해 특수작물 재배
-                및 시설영농이 가 능하도록 10~20ha 규모의 단지에 생산기반시설을 구축한 다음, 인접 시 군의 청년농업인에게 임대·분양<strong>(1인당 상한 규모 설정)</strong>하는 것을
-                목적 으로 설계되었습니다.
-                다만, 논에서의 벼재배는 해당 사업의 지원대상으 로 고려하지 않고 있습니다. 아래의 설문문항을 통해 농업 스타트업단지 에 대한 귀하의 견해를 듣고자 합니다.
+                농업 스타트업단지는 <strong><em>청년농업인의 영농기반을 마련하여 농촌에 안정적으로 정착하고 농지확보 어려움을 해결하고자,
+                저활용 농지(유휴농지, 국·공유지 및 매입비축농지)를 활용해 특수작물 재배 및 시설영농이 가능하도록 10~20ha 규모의 단지에 생산기반시설을 구축</em></strong>한 다음,
+                인접 시군의 청년농업인에게 임 대·분양(1인당 상한 규모 설정)하는 것을 목적으로 설계되었습니다. 다만, 논에서의 벼재배는 해당 사업의 지원대상으로 고려하지 않고 있습니다.
+                아래 의 설문문항을 통해 농업 스타트업단지에 대한 귀하의 견해를 듣고자 합니다.
             </TextBlock>
 
 
@@ -307,7 +310,7 @@ const Sector3 = () => {
                         <Grid gridColumnProperty={"repeat(5,1fr)"}>
                             <FormControlLabel value='1' control={<Radio/>} label='매우 필요함'/>
                             <FormControlLabel value='2' control={<Radio/>} label='대체로 필요함'/>
-                            <FormControlLabel value='3' control={<Radio/>} label='모르겠음'/>
+                            <FormControlLabel value='3' control={<Radio/>} label='보통'/>
                             <FormControlLabel value='4' control={<Radio/>} label={<div>그다지<br/>필요없음</div>}/>
                             <FormControlLabel value='5' control={<Radio/>} label='전혀 필요 없음'/>
                         </Grid>
@@ -501,7 +504,7 @@ const Sector3 = () => {
                     </Grid>
 
                     <Grid strict gridColumnProperty={'5fr 0.5fr 5fr'}>
-                        <CenterBox start>4. 인력 기술 및 정보 획득 용이</CenterBox>
+                        <CenterBox start>4. 인력 및 근로·복지 지원</CenterBox>
                         <div></div>
                         <RadioGroup onChange={handleS3b3_4}>
                             <Grid strict gridColumnProperty={'repeat(5, 1fr)'} columnGap={27}>
@@ -599,7 +602,7 @@ const Sector3 = () => {
                     </Grid>
 
                     <Grid strict gridColumnProperty={'5fr 0.5fr 5fr'}>
-                        <CenterBox start>11. 용이한 용·배수 여견</CenterBox>
+                        <CenterBox start>11. 용이한 용·배수 여건</CenterBox>
                         <div></div>
                         <RadioGroup onChange={handleS3b3_11}>
                             <Grid strict gridColumnProperty={'repeat(5, 1fr)'} columnGap={27}>
@@ -622,7 +625,7 @@ const Sector3 = () => {
                         <Grid gridColumnProperty={"repeat(5,1fr)"}>
                             <FormControlLabel value='1' control={<Radio/>} label={<div>적극적으로<br/>입주할 것임</div>}/>
                             <FormControlLabel value='2' control={<Radio/>} label={<div>입주를 고려할<br/>것 같음</div>}/>
-                            <FormControlLabel value='3' control={<Radio/>} label={'모르겠음'}/>
+                            <FormControlLabel value='3' control={<Radio/>} label={'보통'}/>
                             <FormControlLabel value='4' control={<Radio/>} label={<div>임주를 고려할<br/>것 같지 않음</div>}/>
                             <FormControlLabel value='5' control={<Radio/>} label={'전혀 없음'}/>
                         </Grid>
@@ -654,10 +657,10 @@ const Sector3 = () => {
                         <>
                             <div>3-4-2. 귀하께서는 농업 스타트업단지에 입주하신다면, 재배를 희망하는 작물 부문과 영농형태는 무엇입니까?</div>
                             <br/>
-                            <strong><em>※ 아래 4개 질문(재배희망작물, 영농형태, 영농계획 규모, 스마트팜 여부)에 모두 답변해주세요.</em></strong>
+                            <strong><em>※ 아래 4개 질문(1순위 재배 희망작물, 희망 영농형태, 영농계획 규모, 스마트팜 추진 여부)에 모두 답변해주세요.</em></strong>
                         </>}
                     >
-                        <QuestionBlock title={<ColorCenterBlock>재배희망작물</ColorCenterBlock>}>
+                        <QuestionBlock title={<ColorCenterBlock>1순위 재배 희망작물</ColorCenterBlock>}>
                             <FormControl fullWidth>
                                 <RadioGroup onChange={handleS3b4b2_1}>
                                     <Grid gridColumnProperty={"repeat(5,1fr)"}>
@@ -676,14 +679,14 @@ const Sector3 = () => {
                             </FormControl>
                         </QuestionBlock>
 
-                        <QuestionBlock title={<ColorCenterBlock>영농형태</ColorCenterBlock>}>
+                        <QuestionBlock title={<ColorCenterBlock>희망 영농형태</ColorCenterBlock>}>
                             <FormControl fullWidth>
                                 <RadioGroup onChange={handleS3b4b2_2}>
                                     <Grid gridColumnProperty={"repeat(4,1fr)"}>
                                         <FormControlLabel value='1' control={<Radio/>} label='노지(밭) 재배'/>
                                         <FormControlLabel value='2' control={<Radio/>} label='과수원'/>
-                                        <FormControlLabel value='3' control={<Radio/>} label='단동형 비닐하우스 시설원예'/>
-                                        <FormControlLabel value='4' control={<Radio/>} label='연동형 비닐하우스 시설원예'/>
+                                        <FormControlLabel value='3' control={<Radio/>} label={<CenterBox>시설원예<br/>(단동 비닐하우스)</CenterBox>}/>
+                                        <FormControlLabel value='4' control={<Radio/>} label={<CenterBox>시설원예<br/>(연동 비닐하우스)</CenterBox>}/>
                                     </Grid>
                                 </RadioGroup>
                             </FormControl>
@@ -703,7 +706,7 @@ const Sector3 = () => {
                             </FormControl>
                         </QuestionBlock>
 
-                        <QuestionBlock title={<ColorCenterBlock>스마트팜 여부</ColorCenterBlock>}>
+                        <QuestionBlock title={<ColorCenterBlock>스마트팜 추진 여부</ColorCenterBlock>}>
                             <FormControl fullWidth>
                                 <RadioGroup onChange={handleS3b4b2_4}>
                                     <Grid gridColumnProperty={"repeat(2,1fr)"}>
@@ -750,71 +753,66 @@ const Sector3 = () => {
                             </QuestionBlock>
                         </>
                     }
+
+
+                    <QuestionBlock
+                        title={<div>3-5. 귀하께서는 농업 스타트업단지에 입주하신다면, 거주지에서 몇 분 이내의 통작거리까지 고려할 의 향이 있으십니까? <em>(※ 승용차 편도 기준)</em></div>}>
+                        <FormControl fullWidth>
+                            <RadioGroup onChange={handleS3b5}>
+                                <Grid gridColumnProperty={"repeat(2,1fr)"}>
+                                    <FormControlLabel value='1' control={<Radio/>} label='10분 이내'/>
+                                    <FormControlLabel value='2' control={<Radio/>} label='20분 이내'/>
+                                    <FormControlLabel value='3' control={<Radio/>} label='30분 이내'/>
+                                    <FormControlLabel value='4' control={<Radio/>} label='40분 이내'/>
+                                    <FormControlLabel value='5' control={<Radio/>} label='50분 이내'/>
+                                    <FormControlLabel value='6' control={<Radio/>} label='1시간 이내'/>
+                                    <FormControlLabel value='7' control={<Radio/>} label='통작 시간은 중요하지 않음'/>
+                                </Grid>
+                            </RadioGroup>
+                        </FormControl>
+                    </QuestionBlock>
+
+
+                    <QuestionBlock title={<div>3-6. 농업 스타트업단지가 거주지와 인접한 타 시·군에 설립되어도 <em>상기 문항(3-5)에서</em> 응답한 통작거 리 이내에 위치할 경우, 입주하실 의향은 변함이 없습니까?</div>}>
+                        <FormControl fullWidth>
+                            <RadioGroup onChange={handleS3b6}>
+                                <Grid gridColumnProperty={"repeat(2,1fr)"}>
+                                    <FormControlLabel value='1' control={<Radio/>} label='의향 변함 없음 (입주할 것임)'/>
+                                    <FormControlLabel value='2' control={<Radio/>} label='의향 변함 (입주하지 않을 것임)'/>
+                                </Grid>
+                            </RadioGroup>
+                        </FormControl>
+                    </QuestionBlock>
+
+
+                    <QuestionBlock title={<div>
+                        3-7. 스타트업 단지 내 아래와 같은 단동형·연동형 비닐하우스를 설치할 수 있다면(철골 유리·경질 판·비닐 온실은 설치 불가), 자부담 비율은 어느 정도가 적정하다고 생각하십니까? <strong><em>(※자부담 외는 국고·지방비 보조)</em></strong>
+                    </div>}>
+                        <FormControl fullWidth>
+                            <RadioGroup onChange={handleS3b7}>
+                                <Grid gridColumnProperty={"repeat(5,1fr)"}>
+                                    <FormControlLabel value='1' control={<Radio/>} label='30%'/>
+                                    <FormControlLabel value='2' control={<Radio/>} label='40%'/>
+                                    <FormControlLabel value='3' control={<Radio/>} label='50%'/>
+                                    <FormControlLabel value='4' control={<Radio/>} label='60%'/>
+                                    <FormControlLabel value='5' control={<Radio/>} label='70%'/>
+                                </Grid>
+                            </RadioGroup>
+
+                            <Grid strict gridColumnProperty={"repeat(2, 1fr)"} columnGap={10} rowGap={5} margin={'20px 0px'}>
+                                <CenterBox>단동형 비닐 온실 (설치비: 약 8억/ha)</CenterBox>
+                                <CenterBox>연동형 비닐 온실 (설치비: 약 13억/ha)</CenterBox>
+
+                                <GridImg src={'images/3-7_1.png'}/>
+                                <GridImg src={'images/3-7_2.png'}/>
+
+                                <div></div>
+                            </Grid>
+                            <div>※ 설치비용은 참고사항이며 온실사양에 따라 달라질 수 있음.</div>
+                        </FormControl>
+                    </QuestionBlock>
                 </>
             }
-
-
-            <QuestionBlock
-                title={<div>3-5. 귀하께서는 농업 스타트업단지에 입주하신다면, 거주지에서 몇 분 이내의 통작거리까지 고려할 의 향이 있으십니까? <em>(※ 승용차 편도 기준)</em></div>}>
-                <FormControl fullWidth>
-                    <RadioGroup onChange={handleS3b5}>
-                        <Grid gridColumnProperty={"repeat(2,1fr)"}>
-                            <FormControlLabel value='1' control={<Radio/>} label='10분 이내'/>
-                            <FormControlLabel value='2' control={<Radio/>} label='20분 이내'/>
-                            <FormControlLabel value='3' control={<Radio/>} label='30분 이내'/>
-                            <FormControlLabel value='4' control={<Radio/>} label='40분 이내'/>
-                            <FormControlLabel value='5' control={<Radio/>} label='50분 이내'/>
-                            <FormControlLabel value='6' control={<Radio/>} label='1시간 이내'/>
-                            <FormControlLabel value='7' control={<Radio/>} label='통작 시간은 중요하지 않음'/>
-                            <Stack direction='row'>
-                                <FormControlLabel control={<Radio/>} label='7. 기타'/>
-                                <TextField value={s3b5_Etc8} onChange={(e) => setS3b5_Etc8(e.target.value)}
-                                           size={"small"}/>
-                            </Stack>
-                        </Grid>
-                    </RadioGroup>
-                </FormControl>
-            </QuestionBlock>
-
-
-            <QuestionBlock title={<div>3-6. 농업 스타트업단지가 거주지와 인접한 타 시·군에 설립되어도 <em>상기 문항(3-5)에서</em> 응답한 통작거 리 이내에 위치할 경우, 입주하실 의향은 변함이 없습니까?</div>}>
-                <FormControl fullWidth>
-                    <RadioGroup onChange={handleS3b6}>
-                        <Grid gridColumnProperty={"repeat(2,1fr)"}>
-                            <FormControlLabel value='1' control={<Radio/>} label='의향 변함 없음 (입주할 것임)'/>
-                            <FormControlLabel value='2' control={<Radio/>} label='의향 변함 (입주하지 않을 것임)'/>
-                        </Grid>
-                    </RadioGroup>
-                </FormControl>
-            </QuestionBlock>
-
-
-            <QuestionBlock title={<div>
-                3-7. 스타트업 단지 내 아래와 같은 단동형·연동형 비닐하우스를 설치할 수 있다면(철골 유리·경질 판·비닐 온실은 설치 불가), 자부담 비율은 어느 정도가 적정하다고 생각하십니까?</div>}>
-                <FormControl fullWidth>
-
-                    <Grid strict gridColumnProperty={"repeat(2, 1fr)"} columnGap={10} rowGap={5} margin={'20px 0px'}>
-                        <CenterBox>단동형 비닐 온실 (설치비: 약 8억/ha)</CenterBox>
-                        <CenterBox>연동형 비닐 온실 (설치비: 약 13억/ha)</CenterBox>
-
-                        <GridImg src={'images/3-7_1.png'}/>
-                        <GridImg src={'images/3-7_2.png'}/>
-
-                        <div></div>
-                    </Grid>
-                    <div>※ 설치비용은 참고사항이며 온실사양에 따라 달라질 수 있음.</div>
-
-                    <RadioGroup onChange={handleS3b7}>
-                        <Grid gridColumnProperty={"repeat(5,1fr)"}>
-                            <FormControlLabel value='1' control={<Radio/>} label='30%'/>
-                            <FormControlLabel value='2' control={<Radio/>} label='40%'/>
-                            <FormControlLabel value='3' control={<Radio/>} label='50%'/>
-                            <FormControlLabel value='4' control={<Radio/>} label='60%'/>
-                            <FormControlLabel value='5' control={<Radio/>} label='70%'/>
-                        </Grid>
-                    </RadioGroup>
-                </FormControl>
-            </QuestionBlock>
 
 
             <SectorFooter sector={3} movePreviousSector={movePreviousSector} moveNextSector={moveNextSector} alertOpen={alertOpen} setAlertOpen={setAlertOpen}/>
